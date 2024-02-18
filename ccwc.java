40e72073f94e3dd753cb0127ad0ce5bcc9283fba
Hemant-Dev <phemant092@gmail.com>
2024-02-18 20:06:13 +0530

    Added new command of "-w" that counts the no of words in the txt file.

--- ccwc.java
+++ ccwc.java
@@ -1,8 +1,11 @@
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class ccwc {
+    private static Scanner sc;
     public static void main(String[] args) throws FileNotFoundException{
         String command = args[0];
         String fileName = args[1];
@@ -18,18 +21,36 @@ public class ccwc {
             case "-l":
                 System.out.println("Lines: " + getNoOfLines(file));
                 break;
+            case "-w":
+                System.out.println("Words: " + getNoOfWords(file));
             default:
                 break;
         }
     }
     private static long getNoOfLines(File file) throws FileNotFoundException{
         long noOfLines = 0;
-        Scanner sc = new Scanner(file);
+        sc = new Scanner(file);
         while(sc.hasNextLine()){
             noOfLines++;
             sc.nextLine();
         }
         sc.close();
         return noOfLines;
+    }
+    private static long getNoOfWords(File file) throws FileNotFoundException{
+        long words = 0;
+        sc = new Scanner(new FileInputStream(file));
+        try {
+            FileWriter outFile = new FileWriter("output.txt");
+            while(sc.hasNext()){
+                outFile.write(sc.next() + " || ");
+                words++;
+            }
+            outFile.close();
+        }
+        catch(Exception e){
+            System.out.println("Exception Occurs");
+        }
+        return words;
     }
 }
